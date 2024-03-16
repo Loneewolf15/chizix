24371e82976e67d3c53370b59544d3b6a01e43c9
Loneewolf15 <loneewolf15@protonmail.com>
2024-03-16 09:25:59 -0400

    newed

---
+++ android/app/src/main/java/com/veluxpay/VeluxPay/MainActivity.java
@@ -0,0 +1,14 @@
+package com.veluxpay.VeluxPay;
+
+import android.os.Bundle;
+import com.epicshaggy.biometric.NativeBiometric;
+import com.getcapacitor.BridgeActivity;
+
+
+public class MainActivity extends BridgeActivity {
+    @Override
+     public void onCreate(Bundle savedInstanceState) {
+         registerPlugin(NativeBiometric.class);
+         super.onCreate(savedInstanceState);
+     }
+}
